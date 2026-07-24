7367682eeb4a9c6d6bfb389502a6486dd326c5cb
ajaynxt <ajaynxt2004@gmail.com>
2026-07-24 13:05:05 +0530

    Add files via upload

---
+++ site-config.js
@@ -0,0 +1,8 @@
+window.AJAY_NXT_CONFIG = {
+  weddingShedding: {
+    website: "https://weddingshedding.github.io/._/",
+    googleBusiness: "https://share.google/v7xGQRWmTwm0u1j0X",
+    instagram: "https://www.instagram.com/weddingshedding/",
+    facebook: ""
+  }
+};
